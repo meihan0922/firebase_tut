634cd3f93b9ba6afdb2b1c0ba6c405735fdd60d4
Mei <meihen0922@gmail.com>
2020-11-09 11:43:48 +0800

    auth: 登入狀態下，可以新增文章的功能，並且把取得資料改成realtime

--- scripts/auth.js
+++ scripts/auth.js
@@ -2,16 +2,35 @@
 auth.onAuthStateChanged((user) => {
   // 只有在登入狀態下才能取得資料庫內容，同時資料庫也會去阻擋擁有id的人才能讀寫資料
   if (user) {
-    db.collection("guides")
-      .get()
-      .then((snapshot) => {
-        setupGuides(snapshot.docs);
-      });
+    // 及時顯示! .onSnapshot就像是對當下資料做快照，而後發生改變就去比對，只要不一樣就會觸發
+    db.collection("guides").onSnapshot((snapshot) => {
+      setupGuides(snapshot.docs);
+    });
     setUpUI(user);
   } else {
     setupGuides([]);
     setUpUI();
   }
+});
+
+// create new guide
+const createForm = document.querySelector("#create-form");
+createForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  db.collection("guides")
+    .add({
+      content: createForm["content"].value,
+      title: createForm["title"].value,
+    })
+    .then(() => {
+      // close modal
+      M.Modal.getInstance(modal).close;
+      createForm.reset();
+    })
+    .catch((err) => {
+      // 有人想用前端改寫資料庫就會報錯
+      console.log(err.message);
+    });
 });
 
 // 註冊
